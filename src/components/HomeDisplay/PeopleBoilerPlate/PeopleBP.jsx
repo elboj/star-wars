0d663a27af5a2ec094409b691936c321b88ac734
elboj <adaja.tobi1@gmail.com>
2021-07-01 02:02:40 +0100

    added people section grid

--- src/components/HomeDisplay/PeopleBoilerPlate/PeopleBP.jsx
+++ src/components/HomeDisplay/PeopleBoilerPlate/PeopleBP.jsx
@@ -12,7 +12,31 @@ import {
 } from "@material-ui/core";
 
 const PeopleBP = () => {
-  return <Container maxWidth="md" className="people-container"></Container>;
+  return (
+    <Container maxWidth="md" className="people-container">
+      <Grid container spacing={0}>
+        <Grid item xs={12} sm={6} md={6}>
+          <Card className="people-card">
+            <div className="person-details">
+              <CardContent className="person-content">
+                <Typography component="h5" variant="h5">
+                  Live From Space
+                </Typography>
+                <Typography variant="subtitle1" color="textSecondary">
+                  Mac Miller
+                </Typography>
+              </CardContent>
+            </div>
+            <CardMedia
+              className="cover"
+              image="https://source.unsplash.com/random"
+              title="Live from space album cover"
+            />
+          </Card>
+        </Grid>
+      </Grid>
+    </Container>
+  );
 };
 
 export default PeopleBP;
