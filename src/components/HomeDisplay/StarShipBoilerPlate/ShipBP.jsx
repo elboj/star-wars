c382515a2c303cc164fa066bfd050a379cf1ac14
elboj <adaja.tobi1@gmail.com>
2021-07-01 04:43:48 +0100

    refactored ship grid code structure

--- src/components/HomeDisplay/StarShipBoilerPlate/ShipBP.jsx
+++ src/components/HomeDisplay/StarShipBoilerPlate/ShipBP.jsx
@@ -1,13 +1,10 @@
 import React from "react";
 import { Grid, Container, Button } from "@material-ui/core";
-import useStyles from "./style";
 import { ssImages } from "../../../data/imgSS";
 import "../../../scss/_shipBP.scss";
 import CardShip from "./CardShip";
 
-const card = [0, 1, 2, 3, 4, 5];
-
-const ShipBP = ({ starShip }) => {
+const ShipBP = () => {
   return (
     <Container maxWidth="md" className="starship-container">
       <Grid container spacing={4}>
