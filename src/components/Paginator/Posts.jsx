1b617b1bc56b4bb246189d677148e5de479e28e5
elboj <adaja.tobi1@gmail.com>
2021-07-04 19:26:03 +0100

    post click fix

--- src/components/Paginator/Posts.jsx
+++ src/components/Paginator/Posts.jsx
@@ -17,46 +17,69 @@ import {
 import "../../scss/_posts.scss";
 import useStyles from "../HomeDisplay/People/styles";
 
-function Posts({ setHide, setPG, setPHide, searchResult }) {
+function Posts({
+  people,
+  starShip,
+  search,
+  setHide,
+  setPG,
+  setPHide,
+  searchResult,
+}) {
   const classes = useStyles();
   return (
-    <Container maxWidth="md" className="post-container">
-      <Grid container>
-        {searchResult.map((result, index) => {
-          let title = Object.keys(result);
-          let answer = Object.values(result);
-          return (
-            <Grid key={index} item xs={12} sm={6} md={6}>
-              {title.map((x, index) => {
-                return (
-                  <Card className={`${searchResult.length} ? post-card : null`}>
-                    <CardContent>
-                      <Typography variant="subtitle1">
-                        {x} -- {answer[index]}
-                      </Typography>
-                    </CardContent>
-                  </Card>
-                );
-              })}
-            </Grid>
-          );
-        })}
-        <CardActions>
-          <Button
-            size="large"
-            variant="contained"
-            color="secondary"
-            onClick={() => {
-              setHide(true);
-              setPHide(true);
-              setPG(false);
-            }}
-          >
-            Back to Home
-          </Button>
-        </CardActions>
-      </Grid>
-    </Container>
+    <div>
+      <Container maxWidth="md" className="post-container">
+        <Grid container>
+          <Grid item xs={12} sm={6} md={12}>
+            <Card className={`${searchResult.length} ? post-card : null`}>
+              <CardActionArea>
+                <CardContent>
+                  <Typography variant="h5">Search Results</Typography>
+                  <Typography>{searchResult.length} result found</Typography>
+                </CardContent>
+                <CardMedia
+                  className="cover"
+                  height="140"
+                  image={
+                    searchResult.length > 0
+                      ? "https://source.unsplash.com/random"
+                      : { unknown }
+                  }
+                />
+                {searchResult.map((result, index) => {
+                  let title = Object.keys(result);
+                  let answer = Object.values(result);
+                  return title.map((x, index) => {
+                    return (
+                      <CardContent>
+                        <Typography variant="subtitle1">
+                          {x} -- {answer[index]}
+                        </Typography>
+                      </CardContent>
+                    );
+                  });
+                })}
+              </CardActionArea>
+              <CardActions>
+                <Button
+                  size="large"
+                  variant="contained"
+                  color="secondary"
+                  onClick={() => {
+                    setHide(true);
+                    setPHide(true);
+                    setPG(false);
+                  }}
+                >
+                  Back to Home
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+        </Grid>
+      </Container>
+    </div>
   );
 }
 
